7fa1b0ce4dd375136ef774a89dc4cc314f182b8d
Dennis Traub <dennis.traub@gmail.com>
2013-04-27 13:47:28 +0200

    Added first test

---
+++ src/de/dennistraub/lptdd/SavingsAccount.java
@@ -0,0 +1,19 @@
+package de.dennistraub.lptdd;
+
+public class SavingsAccount {
+
+	private int balance = 0;
+	
+	public void deposit(int amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(int amount) {
+		balance -= amount;
+	}
+
+	public int balance() {
+		return balance;
+	}
+
+}
